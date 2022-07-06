75e4b03f1e59e4a7a7b5c431dec6daf30b86425e
Dmuyto <Voloshynd13@gmail.com>
2022-07-06 11:27:51 +0200

    added new method

--- script.js
+++ script.js
@@ -41,3 +41,130 @@ const numbers = [4, 4, 6, 7, 12, 3, 6];
 const strs = ['apple', 'plum', 'melon']; 
  const res1 = strs.reduce((next, previuos) => next +', ' + previuos)
  console.log(res1);
+
+
+ // Object.entries()
+ const object1 = {
+    a: 'somestring',
+    b: 42
+  };
+  
+  for (const [key, value] of Object.entries(object1)) {
+    console.log( `${key}: ${value}`);
+    console.log(typeof key, value);
+  };
+
+const obj = { foo: "bar", baz: 42 };
+console.log(Object.entries(obj));
+  
+// Itearcja po obiektach za pomoca Object.entries() i metody forEach() 
+
+const object = { foo: 'bar', baz: 42 };
+Object.entries(object).forEach(([key, value]) => console.log(`${key}: ${value}`));
+
+const object2 = {
+    Ivan: 'person',
+    Ann: 'person',
+    dog: 'animal', 
+    cat: 'animal' 
+};
+
+
+// Object.entries(object2).forEach(([key]) => console.log(key)); 
+const newPersonsNames = []; 
+ const newPersons = Object.entries(object2).filter((item)=> {
+    if(item[1] === 'person'){
+        newPersonsNames.push(item[0]);
+     
+    }});
+
+    console.log(newPersonsNames);
+
+
+    const films = [
+        {
+            name: 'Titanic',
+            rating: 9
+        },
+        {
+            name: 'Die hard 5',
+            rating: 5
+        },
+        {
+            name: 'Matrix',
+            rating: 8
+        },
+        {
+            name: 'Some bad film',
+            rating: 4
+        }
+    ];
+    // const newArr = []; 
+    // function showGoodFilms(arr) {
+      
+    //    arr.filter(item =>{
+    //     if(item.rating >= 8 ) newArr.push(item)
+    //    }
+    //    )
+    //    console.log(newArr);
+    // };
+
+    // showGoodFilms(films); 
+
+
+    //1.
+//     function showListOfFilms(arr) {
+//    const listOfFilms = arr.map(item => item.name).join(', ');
+//    console.log(typeof listOfFilms);
+//     }
+
+//     showListOfFilms(films);
+
+
+//2.
+    // function showListOfFilms(arr) {
+    //     const listOfFilms = arr.map(item => item.name).toString();
+    //     console.log(  listOfFilms);
+    //      }
+     
+    //      showListOfFilms(films);
+
+//3.
+
+        //  function showListOfFilms(arr) {
+        //     const listOfFilms =  arr.reduce((acc, curr) =>`${typeof(acc) === 'object' ? acc.name : acc}, ${curr.name}`);
+        //     console.log(listOfFilms)
+        // };
+
+        // showListOfFilms(films) 
+
+// 1.
+        // function setFilmsIds(arr) {
+        //     let idx = 0; 
+        //     const listOfFilms = arr.map(item => Object.assign(item, {id: `${idx++}`}))
+        //     console.log(listOfFilms);
+        // };
+
+        // setFilmsIds(films)
+
+//2.
+
+function setFilmsIds(arr) {
+    return arr.map((film, i) => { // metoda map() przyjmuje dwa argumenty 
+        film.id = i;
+        return film;
+    });
+}
+setFilmsIds(films);
+
+
+// 1.
+
+
+const tranformedArray = setFilmsIds(films);
+
+function checkFilms(arr) {
+    console.log(arr.every(film => film.id || film.id === 0 ? `${film.id} true` : false))
+}
+
+checkFilms(tranformedArray);
